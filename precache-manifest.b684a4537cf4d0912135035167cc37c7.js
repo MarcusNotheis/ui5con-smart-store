5f2d22c5ea0b4a2d3ceeb6cb8ee6cf8286701270
Marcus Notheis <marcus.notheis@sap.com>
2019-07-06 09:27:08 +0200

    Updates

---
+++ precache-manifest.b684a4537cf4d0912135035167cc37c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5556ae3b49fca11502a81d600080c1d2",
+    "url": "/ui5con-smart-store/index.html"
+  },
+  {
+    "revision": "6b1154a182842b53208c",
+    "url": "/ui5con-smart-store/static/css/main.d839cec1.chunk.css"
+  },
+  {
+    "revision": "48a241f10f8f28e17614",
+    "url": "/ui5con-smart-store/static/js/2.0097e4f1.chunk.js"
+  },
+  {
+    "revision": "6b1154a182842b53208c",
+    "url": "/ui5con-smart-store/static/js/main.846aa568.chunk.js"
+  },
+  {
+    "revision": "b0167a04ad30f6d49826",
+    "url": "/ui5con-smart-store/static/js/runtime~main.6be4a9c8.js"
+  },
+  {
+    "revision": "60a8874b9dad44ac35b22643daa14b25",
+    "url": "/ui5con-smart-store/static/media/logo.60a8874b.png"
+  },
+  {
+    "revision": "c531930855aa352961cf62f5b113c5cc",
+    "url": "/ui5con-smart-store/static/media/profile.c5319308.png"
+  }
+]);
